c8f1a4dc44677f3bcdf14ae0b8c61313785f8bbb
akabrio <42409753+akabrio@users.noreply.github.com>
2024-05-09 21:39:53 +0200

    Update main.tf

--- infrastructure/modules/storage-account/main.tf
+++ infrastructure/modules/storage-account/main.tf
@@ -15,6 +15,7 @@ resource "azurerm_storage_account" "st" {
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  account_kind             = "StorageV2"
   is_hns_enabled           = var.hns_enabled
 
   tags = var.tags
